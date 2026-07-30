99bb4d39100eeca68a0f2b66a8661cf32d7aaa7e
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-07-30 12:12:55 +0545

    Socket and Connecting Request is Added For Client

---
+++ Code/client.cpp
@@ -0,0 +1,65 @@
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+    WSADATA wsaData;
+
+    if(WSAStartup(MAKEWORD(2,2), &wsaData)!=0)
+    {
+        std::cout<<"WSAStartup Failed"<<std::endl;
+        return 1;
+    }
+
+    std::cout<<"Winsock Initialized"<<std::endl;
+
+    //Socket Creation
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if(clientSocket == INVALID_SOCKET)
+    {
+        std::cout<<"Socket creation failed"<<std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+
+    serverAddr.sin_family = AF_INET;
+
+    serverAddr.sin_port = htons(54000);
+
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    //Connection Request
+    int result = connect(
+                    clientSocket,
+                    (sockaddr*)&serverAddr,
+                    sizeof(serverAddr)
+                 );
+
+    if(result == SOCKET_ERROR)
+    {
+        std::cout<<"Connection Failed"<<std::endl;
+
+        closesocket(clientSocket);
+        WSACleanup();
+
+        return 1;
+    }
+
+    std::cout<<"Connected to Server"<<std::endl;
+
+    closesocket(clientSocket);
+
+    WSACleanup();
+
+    return 0;
+}
